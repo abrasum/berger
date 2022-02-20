cb0b6a397725bc7bc46815779e43c59832d27416
Valar Morgulis <abrasum@yandex.ru>
2022-02-20 19:49:22 +0300

    next

--- python/berger.py
+++ python/berger.py
@@ -1,22 +1,35 @@
 import csv
 import argparse
 
+table = []
 
-# with open('588399947422-aws-billing-detailed-line-items-2015-09.c‌​sv') as csvfile: 
-#    readCSV = csv.reader(csvfile,delimiter=',') 
-#    for row in readCSV : 
-#        print row
+with open('table1.csv') as csvfile: 
+    readCSV = csv.reader(csvfile,delimiter=',')
+    for row in readCSV:
+        table.append(row)
+
+count_players = len(table[1:])
+count_col_table = len(table[0][0])
+summary_list = []
+
+for row in table[1:]:
+    summary_list.append(float(row[len(row) - 1]))
+
+print(summary_list)
+
+berger_list = []
 
 
+for row in table[1:]:
+    player_id = 0
+    brg = 0
+    for col in row[2:len(row) - 2]:
+        summary = summary_list[player_id]
+        if col == "1":
+            brg = brg + summary
+        if col == "0.5" or col == "0,5" or col == '1/2':
+            brg = brg + summary/2
+        player_id += 1
+    berger_list.append(brg)
 
-# Instantiate the parser
-parser = argparse.ArgumentParser(description='Optional app description')
-parser.add_argument('file', help='A required file path')
-
-args = parser.parse_args()
-file_path = args.file
-
-def read_csv():
-  print("test")
-
-read_csv()
+print(berger_list)
